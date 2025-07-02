dd4c20f990466307e4d7396eab2a784fc7e84871
Kagan Candogan <kagan.candogan@venhancer.com>
2025-07-02 16:43:54 +0300

    fix

--- src/screens/LandingScreen/LandingScreen.tsx
+++ src/screens/LandingScreen/LandingScreen.tsx
@@ -158,8 +158,8 @@ const LandingScreen = ({navigation}: any) => {
                   const month = (date.getMonth() + 1)
                     .toString()
                     .padStart(2, '0');
-                  const year = date.getFullYear().toString();
-                  const formattedDate = `${day} ${month} ${year}`;
+                  const year = date.getFullYear();
+                  const formattedDate = `${day}/${month}/${year}`;
                   handleSelect(index, formattedDate);
                 }
               }}
